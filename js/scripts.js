3cd694795d94de8276b410c2845f2bddda918175
yomZsamora <samora.y@adzumi.co.ke>
2021-04-14 06:32:23 +0300

    jQuery. Adding and Removing Classes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,34 @@
-// UI Logic
-function getDayBorn() {
-    alert("Hi there! We'll start by providing the day born. Click ok to continue");
-    let dateString = prompt("Enter date born in the following format: mm/dd/yyyy"); // Getting the date as a string
-    let gender = prompt("What is your gender, female or male?");
-    getNameFromDate(dateString, gender);
-    
-    
-}
+// $(selector).action()
 
-// Business Logic
-function getNameFromDate(dateString, gender) {
-    let dateOfBirth = new Date(dateString); // Creating a date object from the string
-    let dayOfTheWeek = dateOfBirth.getDay(); // Get the position of the day
-    let maleNames = ["Brian", "Paul", "Isaac", "Kevin", "Gabriel", "Peter", "Timothy"];
-    let femaleNames = ["Susan", "Mary", "Catherine", "Fiona", "Rose", "Stella", "Vivian"];
-    if(gender == "male") {
-        // console.log(maleNames[dayOfTheWeek]);
-        document.getElementById('displayNameHere').innerHTML = "Your Birth Name is " + maleNames[dayOfTheWeek];
-    }
-    else if(gender == "female") {
-        // console.log(femaleNames[dayOfTheWeek]);
-        document.getElementById('displayNameHere').innerHTML = "Your Birth Name is " + femaleNames[dayOfTheWeek];
-    }
-    else {
+$(document).ready(function() {
 
-    }
-}
+    $(".wrong-password").hide();
+
+    // Change colors after click of a button
+    $("#colorButton").click(function() {
+        $("#warningMessage").removeClass("text-warning");
+        $("#warningMessage").addClass("text-primary");
+    });
+
+    // Hide element after Hover
+    $("#errorMessage").hover(function() {
+        $(this).hide();
+    });
+
+
+    // Password length error message
+    $("#passwordInput").on('keyup', function(){
+        if($(this).val().length < 6) {
+            $(".wrong-password").show();
+        }
+        else if(($(this).val().length > 6)) {
+            $(".wrong-password").hide();
+        }
+    });
+
+    $("#changeToAlert").on('click', function(){
+        $("p.footer").addClass("alert alert-success");
+    });
+})
+
+
